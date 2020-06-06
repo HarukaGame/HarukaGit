4dcb70b7d401b84551f0736d1ae05737d3792826
HarukaGame <harukagamecreate@gmail.com>
2020-06-06 23:49:42 +0900

    for文に変更

--- Project/Gauss/Project2/Project2/game/Gauss.cpp
+++ Project/Gauss/Project2/Project2/game/Gauss.cpp
@@ -15,23 +15,28 @@ bool Gauss::CalcEquatons(glm::mat3& _a, glm::vec3& _b, glm::vec3& _solution)
 		for (; k < 3 && tempMat[i][k] == 0; k++);
 		if (k == 3) { return false; }
 		if (i != k) {
-			SwapValue(tempMat[0][i], tempMat[0][k]);
-			SwapValue(tempMat[1][i], tempMat[1][k]);
-			SwapValue(tempMat[2][i], tempMat[2][k]);
+			for (int swapCol = 0; swapCol < 3; swapCol++) {
+				SwapValue(tempMat[swapCol][i], tempMat[swapCol][k]);
+			}
 			SwapValue(_solution[i], _solution[k]);
 		}
 
-		float temp = tempMat[i][i];
-		tempMat[0][i] /= temp;
-		tempMat[1][i] /= temp;
-		tempMat[2][i] /= temp;
-		_solution[i] /= temp;
+		float temp = 1/tempMat[i][i];
+		for (int s = 0; s < 3; s++) {
+			if (i == s) {
+				tempMat[s][i] = 1;
+			}
+			else {
+				tempMat[s][i] *= temp;
+			}
+		}
+		_solution[i] *= temp;
 		for (int j = 0; j < 3; j++) {
 			if (i != j) {
 				float temp2 = tempMat[i][j];
-				tempMat[0][j] -= tempMat[0][i] * temp2;
-				tempMat[1][j] -= tempMat[1][i] * temp2;
-				tempMat[2][j] -= tempMat[2][i] * temp2;
+				for (int Col = 0; Col < 3; Col++) {
+					tempMat[Col][j] -= tempMat[Col][i] * temp2;
+				}
 				_solution[j] -= _solution[i] * temp2;
 			}
 		}
