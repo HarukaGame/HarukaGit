32978de12ed477b7d0167d56b84f5327a9741112
HarukaGame <harukagamecreate@gmail.com>
2020-07-16 00:11:33 +0900

    FileLoaderProjectの追加

---
+++ Project/FileWriterProject/FileWriterProject/file_writer.cpp
@@ -0,0 +1,52 @@
+﻿#include "file_writer.h"
+#include <stdio.h>
+#include "debug_print.h"
+#  pragma warning(disable: 4996)
+
+namespace PREMIUM {
+
+	bool CFileWriter::OpenFile(const char* _filePath, const FILE_OPEN_MODE _mode) {
+		if (m_isOpened == true) {
+			PRINT("CFileWriter::OpenFile already opened\n");
+			return false;
+		}
+
+		if (_mode == FILE_OPEN_MODE::FILE_OPEN_MODE_OVER_WRITE) {
+			m_pFile = fopen(_filePath, "w");
+		}
+		else if (_mode == FILE_OPEN_MODE::FILE_OPEN_MODE_ADD) {
+			m_pFile = fopen(_filePath, "a");
+		}
+		else {
+			PRINT("CFileWriter::OpenFile _mode is invalid\n");
+			return false;
+		}
+		if (m_pFile == nullptr) {
+			PRINT("CFileWriter::OpenFile open failed\n");
+			return false;
+		}
+		m_isOpened = true;
+		return true;
+	}
+	void CFileWriter::Write(const char* _str){
+		if (m_isOpened == false) {
+			PRINT("CFileWriter::Write file is not opened\n");
+			return;
+		}
+
+		if (m_pFile == nullptr) {
+			PRINT("CFileWriter::Write m_pFile is nullptr\n");
+			return;
+		}
+		fprintf(m_pFile, _str);
+	}
+	void CFileWriter::WriteLine(const char* _str)
+	{
+		Write(_str);
+		Write("\n");
+	}
+	void CFileWriter::CloseFile(){
+		fclose(m_pFile);
+		m_isOpened = false;
+	}
+}
---
+++ Project/FileWriterProject/FileWriterProject/file_writer.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#ifndef __FILE_WRITER_H__
+#define __FILE_WRITER_H__
+
+struct _iobuf;
+typedef struct _iobuf FILE;
+
+namespace PREMIUM {
+	enum class FILE_OPEN_MODE {
+		FILE_OPEN_MODE_OVER_WRITE,
+		FILE_OPEN_MODE_ADD
+	};
+
+	class CFileWriter {
+	private:
+		FILE* m_pFile = nullptr;
+		bool m_isOpened = false;
+	public:
+		bool OpenFile(const char* _filePath, const FILE_OPEN_MODE _mode);
+		void Write(const char* _str);
+		void WriteLine(const char* _str);
+		void CloseFile();
+	};
+
+}
+
+#endif // !__FILE_WRITER_H__
